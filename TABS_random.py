25905a99c0d522d66db8c9c31167aeaaad46e018
floriandelrieu <florian.delrieu@free.fr>
2022-09-08 11:24:35 +0200

    [v0.2] - Special units and output text
    - random libraries modified
    - special units random loop
    - beginning of output user text

--- TABS_random.py
+++ TABS_random.py
@@ -1,4 +1,16 @@
-import numpy.random as rd
+import random as rd
+
+
+
+def choose_random_faction(current_list):
+    current_list.append(rd.choice(Faction_list))
+    if not Allow_doublons:
+        check_doublons(current_list)
+
+def check_doublons(list):
+    if list[-1] in list[:-1] :
+        list.remove(list[-1])
+        choose_random_faction(list)
 
 Faction_list = ["Tribal",
                 "Farmer",
@@ -13,18 +25,8 @@ Faction_list = ["Tribal",
                 "Good",
                 "Evil",
                 ]
-
-def choose_random_faction(current_list):
-    current_list.append(rd.choice(Faction_list))
-    if not Allow_doublons:
-        check_doublons(current_list)
-
-def check_doublons(list):
-    if list[-1] in list[:-1] :
-        list.remove(list[-1])
-        choose_random_faction(list)
-
-Number_of_faction = 10
+Number_of_faction = 2
+Points = 3000
 Allow_doublons = False
 
 Player1_list = []
@@ -32,7 +34,24 @@ Player2_list = []
 
 loop = 1
 while loop <= Number_of_faction:
+    # Player 1
     choose_random_faction(Player1_list)
+    P1_SpecialUnits = rd.choices([True,False],[9,1])[0]
+
+    # Player 2
     choose_random_faction(Player2_list)
+    P2_SpecialUnits = rd.choices([True, False], [9, 1])[0]
+
     loop += 1
 
+print(f"""
+Player 1: 
+- Points :              {Points}
+- Factions choices :    {Player1_list}
+- Special units :       {P1_SpecialUnits} {"(+" + str(0.1*Points) +"pts)" if P1_SpecialUnits == False else ""}
+ 
+ Player 2: 
+- Points :              {Points}
+- Factions choices :    {Player2_list}
+- Special units :       {P2_SpecialUnits} {"(+" + str(0.1*Points) +"pts)" if P2_SpecialUnits == False else ""}
+""")
